10561ea2695f48e94adf0c8075dbb6404d70f26b
masihnajafi02 <najafim2002@gmail.com>
2024-07-29 23:42:44 +0330

    Finish test process tasks

--- TDDLibrary/src/test/java/SearchFeatureTests.java
+++ TDDLibrary/src/test/java/SearchFeatureTests.java
@@ -5,6 +5,8 @@ import classes.Student;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -57,4 +59,115 @@ public class SearchFeatureTests {
         List<Student> result = library.searchStudents(SearchByType.AUTHOR, keys);
         assertEquals(null, result);
     }
+
+
+    @Test
+    public void testLendBook_NotRegisteredBook() {
+        Library library = new Library();
+        Book book = new Book("Book 1", "Author 1", 1);
+        Student student = new Student("Student 1", 1);
+        library.addStudent(student);
+
+        assertFalse(library.lendBook(book, student));
+    }
+
+    @Test
+    public void testLendBook_StudentAlreadyHasBook() {
+        Library library = new Library();
+        Book book = new Book("Book 1", "Author 1", 1);
+        Student student = new Student("Student 1", 1);
+        library.addBook(book);
+        library.addStudent(student);
+        student.addBook(book);
+
+        assertFalse(library.lendBook(book, student));
+    }
+
+    @Test
+    public void testLendBook_StudentNotRegistered() {
+        Library library = new Library();
+        Book book = new Book("Book 1", "Author 1", 1);
+        Student student = new Student("Student 1", 1);
+        library.addBook(book);
+
+        assertFalse(library.lendBook(book, student));
+    }
+
+    @Test
+    public void testReturnBook_StudentNotRegistered() {
+        Library library = new Library();
+        Book book = new Book("Book 1", "Author 1", 1);
+        Student student = new Student("Student 1", 1);
+
+        assertFalse(library.returnBook(book, student));
+    }
+
+    @Test
+    public void testReturnBook_StudentDoesNotHaveBook() {
+        Library library = new Library();
+        Book book = new Book("Book 1", "Author 1", 1);
+        Student student = new Student("Student 1", 1);
+        library.addStudent(student);
+
+        assertFalse(library.returnBook(book, student));
+    }
+
+
+    @Test
+    public void testDisplayBooks() {
+        Library library = new Library();
+        Book book1 = new Book("Book 1", "Author 1", 1);
+        Book book2 = new Book("Book 2", "Author 2", 2);
+        library.addBook(book1);
+        library.addBook(book2);
+
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+
+        library.displayBooks();
+
+        String expectedOutput = "Available books in library:\n" +
+                book1.toString() + "\n" +
+                book2.toString() + "\n";
+
+        String actualOutput = outContent.toString().replace("\r\n", "\n").replace("\r", "\n");
+        expectedOutput = expectedOutput.replace("\r\n", "\n").replace("\r", "\n");
+
+        assertEquals(expectedOutput, actualOutput);
+
+
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testDisplayStudents() {
+        Library library = new Library();
+        Student student1 = new Student("Student 1", 1);
+        Student student2 = new Student("Student 2", 2);
+        library.addStudent(student1);
+        library.addStudent(student2);
+
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+
+        library.displayStudents();
+
+        String expectedOutput = "Registered students:\n" +
+                student1.toString() + "\n" +
+                student2.toString() + "\n";
+
+        String actualOutput = outContent.toString().replace("\r\n", "\n").replace("\r", "\n");
+        expectedOutput = expectedOutput.replace("\r\n", "\n").replace("\r", "\n");
+
+        assertEquals(expectedOutput, actualOutput);
+
+
+        System.setOut(originalOut);
+    }
 }
+
+
